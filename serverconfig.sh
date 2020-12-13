e6cc54634fcf68dd0736884f114f9297bb399539
justinturbeville <59580400+justinturbeville@users.noreply.github.com>
2020-12-13 15:26:00 -0500

    Add files via upload

---
+++ serverconfig.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#Configuration script for Debian 10 servers
+
+apt update -y
+apt upgrade -y
+apt install open-vm-tools -y
+apt install sudo -y
+echo 'deb http://deb.debian.org/debian buster-backports main' > \
+ /etc/apt/sources.list.d/backports.list
+apt update -y 
+apt install -t buster-backports cockpit -y
